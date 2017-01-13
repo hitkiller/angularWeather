343568e29aa91b70fc9461a6e6b9b78974a99231
Anastasiya Samotiya <Anastasiya.Samotiya1@thomsonreuters.com>
2017-01-13 16:59:29 +0300

    minor syntax fix

--- app/meteo/utilities/temp-color-conversion.directiive.ts
+++ app/meteo/utilities/temp-color-conversion.directiive.ts
@@ -1,14 +1,13 @@
-import {Directive, ElementRef} from '@angular/core';
+import {Directive, ElementRef, Input} from '@angular/core';
 
 @Directive({
-    selector: '[bgColor]',
-    inputs: ['bgColor']
+    selector: '[bgColor]'
 })
 export class TempColorDirective {
     constructor(private el: ElementRef) {
         this.el = el;
     }
-    set bgColor(temp: number) {
+    @Input() set bgColor(temp: number) {
         if (temp <= -10) this.el.nativeElement.style.backgroundColor = 'rgba(0, 175, 190, 0.3)';
         else if (temp <= -5 && temp > -10) this.el.nativeElement.style.backgroundColor = 'rgba(0, 175, 190, 0.2)';
         else if (temp < 0 && temp > -5) this.el.nativeElement.style.backgroundColor = 'rgba(0, 175, 190, 0.07)';
