10839cd0c8f6a69d3bcf8b0201470ce6b9839442
Anastasiya Samotiya <Anastasiya.Samotiya1@thomsonreuters.com>
2017-01-20 18:49:12 +0300

    get_meteo function upgrade

--- app/map/geolocation.service.ts
+++ app/map/geolocation.service.ts
@@ -4,18 +4,18 @@ import {Observer, Observable, BehaviorSubject} from 'rxjs';
 @Injectable()
 export class Geolocator {
     public getLocation = function getLocation(opts = { enableHighAccuracy: true, maximumAge: 30000, timeout: 27000 }): BehaviorSubject<any> {
-        let defaultLocation = new BehaviorSubject({ latitude: 53, longitude: 27 });
+        let defaultLocation$ = new BehaviorSubject({ latitude: 53, longitude: 27 });
 
         window.navigator.geolocation.getCurrentPosition((position) => {
-            defaultLocation.next(position.coords);
+            defaultLocation$.next(position.coords);
         },
             (error) => {
-                defaultLocation.error({
+                defaultLocation$.error({
                     1: 'errors.location.permissionDenied',
                     2: 'errors.location.positionUnavailable',
                     3: 'errors.location.timeout'
                 }[error.code]);
             }, opts);
-        return defaultLocation;
+        return defaultLocation$;
     };
 }
--- app/meteo/meteo-cities.component.ts
+++ app/meteo/meteo-cities.component.ts
@@ -37,6 +37,7 @@ export class MeteoCitiesComponent implements OnInit {
     forecast: any[];
     private _units = 'metric';
     private _APPID = '47bc4e43962dbb173c1a3a7b2d5d0aa9';
+    _citiesData$:Observable<any> = null;
 
     public isInFavorites: boolean = false;
 
@@ -58,17 +59,18 @@ export class MeteoCitiesComponent implements OnInit {
     }
 
     getMeteo() {
-        return this.geolocationService.getLocation()
-            .flatMap(pos => this.http.get(`http://api.openweathermap.org/data/2.5/find?lat=${pos.latitude}&lon=${pos.longitude}&cnt=50&units=${this._units}&APPID=${this._APPID}`))
-            .map((res: Response) => {
-              //  setInterval(() => {
+        if (!this._citiesData$) {
+            this._citiesData$ = this.geolocationService.getLocation()
+                .flatMap(pos => this.http.get(`http://api.openweathermap.org/data/2.5/find?lat=${pos.latitude}&lon=${pos.longitude}&cnt=50&units=${this._units}&APPID=${this._APPID}`))
+                .map((res: Response) => {
                     this.cd.markForCheck();
                     this.citiesData = res.json().list;
                     this.forecast = utilDisplayWeatherFunction.displayWeatherData(this.citiesData);
-              //  }, 15000);
-            })
-            .publishReplay(1)
-            .refCount()
-            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+                })
+                .publishReplay(1)
+                .refCount()
+                .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+        }
+        return this._citiesData$;
     }
 }
