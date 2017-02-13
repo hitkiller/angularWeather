95c4f80cebee76cfc8f51130642e7c67e9b93832
Anastasiya Samotiya <Anastasiya.Samotiya1@thomsonreuters.com>
2017-02-13 22:33:31 +0300

    visual updates

--- app/meteo/utilities/temp-color-conversion.directive.ts
+++ app/meteo/utilities/temp-color-conversion.directive.ts
@@ -10,8 +10,8 @@ export class TempColorDirective {
     @Input() set bgColor(temp: number) {
         if (temp <= -10) this.el.nativeElement.style.backgroundColor = 'rgba(0, 175, 190, 0.3)';
         else if (temp <= -5 && temp > -10) this.el.nativeElement.style.backgroundColor = 'rgba(0, 175, 190, 0.2)';
-        else if (temp < 0 && temp > -5) this.el.nativeElement.style.backgroundColor = 'rgba(0, 175, 190, 0.07)';
-        else if (temp > 0 && temp <= 5) this.el.nativeElement.style.backgroundColor = 'rgba(240, 85, 20, 0.07)';
+        else if (temp < 0 && temp > -5) this.el.nativeElement.style.backgroundColor = 'rgba(0, 175, 190, 0.1)';
+        else if (temp > 0 && temp <= 5) this.el.nativeElement.style.backgroundColor = 'rgba(240, 85, 20, 0.1)';
         else if (temp > 5 && temp <= 15) this.el.nativeElement.style.backgroundColor = 'rgba(240, 85, 20, 0.13)';
         else if (temp > 15 && temp <= 25) this.el.nativeElement.style.backgroundColor = 'rgba(240, 85, 20, 0.21)';
         else if (temp > 25) this.el.nativeElement.style.backgroundColor = 'rgba(240, 85, 20, 0.3)';
